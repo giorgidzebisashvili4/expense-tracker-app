abf8e423e1da342af546efda97ee037c8b6d5513
giorgidzebisashvili4 <giorgidzebisashvili4@gmail.com>
2025-02-18 12:31:12 +0400

    improve app layout and styling

--- components/expensesOutput/ExpensesList.js
+++ components/expensesOutput/ExpensesList.js
@@ -1,7 +1,17 @@
-import { FlatList } from "react-native";
+import { FlatList, Text } from "react-native";
+
+function renderExpenseItem(itemData) {
+  return <Text>{itemData.item.description}</Text>;
+}
 
 function ExpensesList({ expenses }) {
-  return <FlatList data={expenses} keyExtractor={(item) => item.id} />;
+  return (
+    <FlatList
+      data={expenses}
+      keyExtractor={(item) => item.id}
+      renderItem={renderExpenseItem}
+    />
+  );
 }
 
 export default ExpensesList;
