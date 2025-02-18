851976c642a11db120a46fb3cd3bec6e3a05a391
giorgidzebisashvili4 <giorgidzebisashvili4@gmail.com>
2025-02-18 11:49:05 +0400

    add global colors and edit navigation configuration

--- App.js
+++ App.js
@@ -6,16 +6,45 @@ import AllExpenses from "./screens/AllExpenses";
 import ManageExpense from "./screens/ManageExpense";
 import RecentExpenses from "./screens/RecentExpenses";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { GlobalStyles } from "./constant/styles";
+import { Ionicons } from "@expo/vector-icons";
 
 export default function App() {
   const Stack = createNativeStackNavigator();
   const BottomTabs = createBottomTabNavigator();
-
   function ExpensesOverview() {
     return (
-      <BottomTabs.Navigator>
-        <BottomTabs.Screen name="RecentExpenses" component={RecentExpenses} />
-        <BottomTabs.Screen name="AllExpenses" component={AllExpenses} />
+      <BottomTabs.Navigator
+        screenOptions={{
+          headerStyle: { backgroundColor: GlobalStyles.colors.primary500 },
+          headerTintColor: "white",
+          tabBarStyle: { backgroundColor: GlobalStyles.colors.primary500 },
+          tabBarActiveTintColor: GlobalStyles.colors.accent500,
+          tabBarActiveBackgroundColor: GlobalStyles.colors.primary700,
+        }}
+      >
+        <BottomTabs.Screen
+          name="RecentExpenses"
+          component={RecentExpenses}
+          options={{
+            title: "Recent Expenses",
+            tabBarLabel: "Recent",
+            tabBarIcon: ({ color, size }) => (
+              <Ionicons name="hourglass" size={size} color={color} />
+            ),
+          }}
+        />
+        <BottomTabs.Screen
+          name="AllExpenses"
+          component={AllExpenses}
+          options={{
+            title: "All Expenses",
+            tabBarLabel: "All Expenses",
+            tabBarIcon: ({ color, size }) => (
+              <Ionicons name="calendar" size={size} color={color} />
+            ),
+          }}
+        />
       </BottomTabs.Navigator>
     );
   }
